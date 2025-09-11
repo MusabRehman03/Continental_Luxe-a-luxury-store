b9e1acb0a0d3f3d0f9e41df27cd83c1a1721e151
Musab Ur Rehman <rehmanmusab0302@gmail.com>
2025-09-11 12:38:17 +0500

    feat: ownersRouter and other with separation of conncers

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const express = require('express')
 const app= express()
 const db = require('./config/mongoose-connection')
 
+const usersRouter = require('./routes/usersRouter')
+const productsRouter = require('./routes/productsRouter')
+const ownersRouter = require('./routes/ownersRouter')
+
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
-app.get('/', (req, res)=>{
-
-})
+app.use('/users', usersRouter)
+app.use('/products', productsRouter)
+app.use('/owners', ownersRouter)
 
 app.listen(3000);
