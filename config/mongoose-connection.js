b774c4b81da6d209f3951b2d0a4e658089799760
Musab Ur Rehman <rehmanmusab0302@gmail.com>
2025-09-11 12:15:44 +0500

    fix: spelling issue in db connnection

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose
 .connect('mongodb://127.0.0.1:27017/Continental-Luxe')
-.then(()=>{console.log('dbb connnected succesfully')})
+.then(()=>{console.log('db connnected succesfully')})
 .catch((err)=>{console.log("error in db connection: ", err)})
 
 module.exports = mongoose.connection;
